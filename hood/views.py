e0545e2695614bfeb1f6b59907f749fc859caae5
AokoMercyline <aokomercyline34@gmail.com>
2020-10-31 21:19:56 +0300

    views for account

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from .forms import SignUpForm, UpdateProfileForm
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
-# Create your views here.
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+    
+
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('profile', user.username)
+    else:
+        form = UpdateProfileForm()
+    return render(request, 'create_profile.html', {'form': form})
+
+
+def profile(request, username):
+    return render(request, 'profile.html')
+
+
+
+
