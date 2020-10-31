38afe17894677bb204981942cbad193a817a82e1
AokoMercyline <aokomercyline34@gmail.com>
2020-10-31 21:24:06 +0300

    hood/urls.py

---
+++ hood/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('profile/<username>', views.profile, name='profile'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
