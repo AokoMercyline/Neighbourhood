10103c518efaa7c0e42f8ae70cd4d2588134ba9b
AokoMercyline <aokomercyline34@gmail.com>
2020-10-31 21:18:41 +0300

    models for profile

---
+++ hood/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=50)
+    email = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        
+        
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user', 'neighbourhood')
